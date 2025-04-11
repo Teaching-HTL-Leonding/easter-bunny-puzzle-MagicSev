64782dacdd99f08e68a41362f676e8816744fa9b
MagicSev <severin.gusenleitner@gmail.com>
2025-04-11 12:39:36 +0200

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import p5 from "p5";
+
 let bunnyImage: p5.Image;
 const p = new p5((sketch) => {
     sketch.setup = setup;
-   sketch.preload = preload;
+    sketch.preload = preload;
     sketch.draw = draw;
 });
+const rows = 2
+const colums = 2
 
 function setup() {
     p.createCanvas(500, 500);
@@ -13,6 +16,16 @@ function preload(){
     bunnyImage = p.loadImage("assets/bunny.png")
 }
 function draw(){
+    const imagewidth = p.width/colums
+    const imageheight = p.height/rows
     p.background("white")
-    p.image(bunnyImage,0,0)
+    p.stroke("white")
+    p.noFill();
+    p.strokeWeight(2)
+    for(let y =0 ; y<colums;y++){
+        for(let x = 0; x<rows;x++){
+            p.image(bunnyImage,x*imagewidth,y*imageheight,imagewidth,imageheight,x*imagewidth,y*imageheight,imagewidth,imageheight)
+            p.rect(x*imagewidth,y*imageheight,imagewidth,imageheight)
+        }
+    }
 }
